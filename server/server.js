310a44df6cc40966fab68edb364beb5d5765ed84
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-10-29 17:01:23 +0000

    Updated server file ready for master merge

--- server/server.js
+++ server/server.js
@@ -81,13 +81,13 @@ app.use("/api/halloween", isLoggedIn, halloweenRoute);
 // Uncomment /* */ when deploying
 // These are for serving production code
 // The directory may need to change
-//app.use(express.static(path.join(__dirname, "../frontend/build"))); */
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 // Necessary since things like /gym do not actually exist they are routes
 // within the index.html file
 
-//app.get('/*', function (req, res) {
-//  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-//});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 
 // Listen for requests on the port specified in the .env file
