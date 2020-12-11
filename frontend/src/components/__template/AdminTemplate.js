b4820d596beaa5c8ba818c6e84a78d6bfe2b8b6c
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-12-11 18:02:22 +0000

    Admin template

---
+++ frontend/src/components/__template/AdminTemplate.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import api from '../../utils/axiosConfig';
+import LoadingHolder from '../common/LoadingHolder';
+
+class AdminTemplatePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false,
+      status: 0,
+      error: ""
+    };
+
+    // Change this to your permission
+    this.requiredPermission = "YourPermissionHere";
+  }
+
+  // Load the data once the element is ready
+  componentDidMount = async () => {
+    let adminCheck;
+
+    try {
+      adminCheck = await api.get("/auth/verify");
+    } catch (error) {
+      this.setState({ status: error.response.status, error: "Unable to verify admin status" });
+      return;
+    }
+
+    // Ensure they are an admin
+    if(adminCheck.data.user.permissions) {
+      if(adminCheck.data.user.permissions.length === 0) {
+        this.setState({ status: 403, error: "You are not an admin" });
+        return;
+      }
+
+      if(!adminCheck.data.user.permissions.includes(this.requiredPermission)) {
+        this.setState({ status: 403, error: "You are not an admin" });
+        return;
+      }
+    } else {
+      this.setState({ status: 403, error: "You are not an admin" });
+      return;
+    }
+
+    // Load any required data for the page here
+
+    this.setState({ loaded: true });
+  }
+
+  render () {
+    if(!this.state.loaded) {
+      if(this.state.status !== 200 && this.state.status !== 0) {
+        return (
+         <Redirect to={`/errors/${this.state.status}`} />
+        );
+      }
+
+      return (
+        <LoadingHolder />
+      );
+    }
+
+    return (
+      <div className="flex flex-col justify-start">
+        <div className="container mx-auto text-center p-4">
+          <h1 className="font-semibold text-5xl pb-4">Title Here</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminTemplatePage;
