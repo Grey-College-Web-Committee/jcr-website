f763f622df83bcd944a087334bce3adf7935d96d
fboyle2001 <finlayboyle2001@gmail.com>
2021-01-24 22:02:22 +0000

    Made some changes to the welfare text and fixed the grid on lower res

--- frontend/src/components/welfare/WelfarePage.js
+++ frontend/src/components/welfare/WelfarePage.js
@@ -137,18 +137,18 @@ class WelfarePage extends React.Component {
 
     return (
       <div className="flex flex-col justify-start">
-        <div className="container mx-auto text-center p-4 md:w-3/5">
+        <div className="mx-auto text-center p-4 md:w-4/5">
           <h1 className="font-semibold text-5xl pb-2">Welfare</h1>
           <div className="text-justify p-2 border-b-2 border-red-900 mb-2">
             <h2 className="text-left font-semibold text-3xl">How do you find us?</h2>
-            <p className="mb-2">We run drop-in hours 3 times a week, in the evening/afternoon, during which anyone can drop in to talk to the team member on duty about anything, in confidence. Welfare hours are held in the Welfare Room which is located on the bottom floor of Hollingside, opposite the laundry room. They are <span className="font-semibold">held on Tuesday and Thursday 6.30-8.30pm and Sunday 2-4pm</span>. You can find out which member of the team is running the drop-ins by looking at the rota which is emailed out to college and will be posted outside the dining hall!</p>
+            <p className="mb-2">We run drop-in hours 3 times a week, in the evening/afternoon, during which anyone can drop in to talk to the team member on duty about anything, in confidence. Welfare hours are held in the Welfare Room which is located on the bottom floor of Hollingside, opposite the laundry room. They are <span className="font-semibold">held on Tuesday and Thursday 6.30-8.30pm and Sunday 2-4pm.</span></p>
             <p className="mb-2">We can also be found <a href="https://www.facebook.com/greycollegewelfare/" target="_blank" rel="noopener noreferrer" className="underline font-semibold">on Facebook</a> and <a href="https://www.instagram.com/greywelfare/" target="_blank" rel="noopener noreferrer" className="underline font-semibold">on Instagram</a>.</p>
             <p className="mb-2">So make sure you all look out for the Welfare team: we are here to support you anytime you need it, to make sure everyone enjoys their university experience as much as they can.</p>
           </div>
           <div className="text-justify p-2 border-b-2 border-red-900 mb-2">
             <h2 className="text-left font-semibold text-3xl">Who are we?</h2>
             <p>Click on our profiles to learn a bit more about each of us!</p>
-            <div className="grid grid-cols-2 gap-2 md:grid-cols-5 md:gap-4 auto-rows-fr">
+            <div className="grid grid-cols-2 gap-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 2xl:gap-4 auto-rows-fr">
               { welfareTeam.map((person, i) => (
                 <PersonElement
                   role={person.role}
@@ -162,7 +162,7 @@ class WelfarePage extends React.Component {
           <div className="flex flex-col md:flex-row text-justify md:border-b-2 md:border-red-900 md:mb-2">
             <div className="p-2 flex-1 border-b-2 border-red-900 mb-2 md:border-none md:mb-0">
               <h2 className="text-left font-semibold text-3xl">Anonymous Online Support</h2>
-              <p className="mb-2">You can use the <Link to="/welfare/message/" className="font-semibold underline">anonymous online support system</Link> to send an anonymous message to the Male and Female Welfare Officers from whom you should receive a reply from within 24 hours. Click here to access this service. You could report an incident (such as egging, drink spiking) to enable us to warn other students about it. You could ask us a question if you prefer not to talk directly. Or you can simply use it as an easy way of sending us your thoughts. Here are a few of the things you might want to write about:</p>
+              <p className="mb-2">You can use the <Link to="/welfare/message/" className="font-semibold underline">anonymous online support system</Link> to send an anonymous message to the Senior Welfare Officers from whom you should receive a reply from within 24 hours. You could report an incident (such as egging, drink spiking) to enable us to warn other students about it. You could ask us a question if you prefer not to talk directly. Or you can simply use it as an easy way of sending us your thoughts. Here are a few of the things you might want to write about:</p>
               <ul className="mb-2">
                 <li>Request information like leaflets, names of charities, phone numbers etc.</li>
                 <li>Tell us you'd like to see more coverage of a certain issue, illness or disability.</li>
@@ -175,8 +175,8 @@ class WelfarePage extends React.Component {
               <h2 className="text-left font-semibold text-3xl">Sexual Health</h2>
               <h3 className="text-left font-semibold text-xl">Pregnancy Tests</h3>
               <p className="mb-2">Free pregnancy tests can be taken from the welfare room at any time during drop in. The test is very simple and only takes a few minutes. Please contact the anonymous online support system if you would like a test out of drop in hours or would like them to help you.</p>
-              <h3 className="text-left font-semibold text-xl">Condoms</h3>
-              <p className="mb-2">Condoms and lube are both free, and are available in a range of sizes and types, including female and latex-free. They can be picked up in drop-ins, from the box outside the Welfare room out of hours, or by messaging the anonymous online support system.</p>
+              <h3 className="text-left font-semibold text-xl">Condoms and Dental Dams</h3>
+              <p className="mb-2">Condoms, lube and dental dams are all free, and are available in a range of sizes and types, including female and latex-free. They can be picked up in drop-ins or by messaging the anonymous online support system.</p>
             </div>
           </div>
           <div className="text-justify">
