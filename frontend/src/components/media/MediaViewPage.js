acc088f69e2213d81a457687309acc4ba432bde6
HackerByName <34354459+HackerByName@users.noreply.github.com>
2021-01-24 22:39:58 +0000

    Delete unneccessary media admin page, add Grey Matter Podcast to view media page, add all media selector to view media page

--- frontend/src/components/media/MediaViewPage.js
+++ frontend/src/components/media/MediaViewPage.js
@@ -14,7 +14,8 @@ class MediaPage extends React.Component {
       loaded: false,
       status: 0,
       error: "",
-      media: []
+      media: [],
+      selectedMediaIndex: null
     };
   }
 
@@ -60,7 +61,6 @@ class MediaPage extends React.Component {
     });
 
     this.setState({ media });
-    console.log(this.state.media);
   }
 
   getPartialUrlFromiFrameString(iframe){
@@ -69,8 +69,13 @@ class MediaPage extends React.Component {
   }
 
   getiFrameFromPartialUrl(partialUrl){
-    return (<iframe scrolling="no" width="100%" frameborder="no" allow="autoplay" src={'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+partialUrl+'&color=%23990000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'}></iframe>)
+    return (<iframe className="rounded-lg w-full h-32" align="center" scrolling="no" frameBorder="no" allow="autoplay" title='podcast' src={'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+partialUrl+'&color=%23990000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'}></iframe>)
   }
+
+  getVideoiFrameFromPartialUrl(partialUrl){
+    return(<iframe src={'https://www.youtube.com/embed/'+partialUrl} className="sm:w-96 sm:h-52 w-64 h-32" frameBorder='no' allow='autoplay; encrypted-media' allowFullScreen title='video'/>)
+  }
+  //https://www.youtube.com/watch?v=mYUcJ0goABM
 
   getLatestJCRPodcast(){
     let latestPodcastID = null;
@@ -82,17 +87,129 @@ class MediaPage extends React.Component {
     }
     if (latestPodcastID != null){
       return (
-        <div className="w-auto align-center bg-red-900 rounded-full my-3 py-3 px-24">
-          <h2 className="font-semibold text-3xl py-3 text-white">Latest JCR Podcast</h2>
-          <div className="w-full">
+        <div className="xl:w-8/12 w-11/12 md:rounded-r-full rounded-r-lg bg-red-900 bg-opacity-90 my-3 py-3 px-5 flex float-left text-left justify-start">
+          <div className="text-left w-11/12 float-left">
+            <h2 className="self-center font-semibold text-3xl py-3 text-white">Latest JCR Podcast</h2>
+            <h2 className="self-center font-semibold text-xl pt-3 pb-1 text-white">{this.state.media[latestPodcastID].mediaTitle}</h2>
             {this.getiFrameFromPartialUrl(this.state.media[latestPodcastID].mediaLink)}
+            <p className="self-center w-auto font-light pb-3 pt-1 text-white flex-shrink">{this.state.media[latestPodcastID].mediaDescription}</p>
           </div>
-          <div className="font-medium text-2xl pt-3 text-white">{this.state.media[latestPodcastID].mediaTitle}</div>
-          <div className="self-center w-auto font-light pb-3 text-white">{this.state.media[latestPodcastID].mediaDescription}</div>
         </div>
       );
     }
-    else { return "Nope" }
+    else { return <></> }
+  }
+
+  getLatestGMPodcast(){
+    let latestPodcastID = null;
+    const len = this.state.media.length;
+    for (var i = 0; i < len; i++){
+      if (this.state.media[i].mediaCategory === "Grey Matter Podcast"){
+        latestPodcastID = i;
+      }
+    }
+    if (latestPodcastID != null){
+      return (
+        <div className="xl:w-8/12 w-11/12 md:rounded-l-full rounded-l-lg bg-gray-400 my-3 py-3 px-5 flex float-right text-right justify-end">
+          <div className="text-right w-11/12 float-right">
+            <h2 className="font-semibold text-3xl py-3 text-gray-900">Latest Grey Matter Podcast</h2>
+            <h2 className="font-semibold text-xl pt-3 pb-1 text-gray-900">{this.state.media[latestPodcastID].mediaTitle}</h2>
+            {this.getiFrameFromPartialUrl(this.state.media[latestPodcastID].mediaLink)}
+            <p className="w-auto font-light pb-3 pt-1 text-gray-900 flex-shrink">{this.state.media[latestPodcastID].mediaDescription}</p>
+          </div>
+        </div>
+      );
+    }
+    else { return <></> }
+  }
+
+  getLatestVideo(){
+    let latestVideoID = null;
+    const len = this.state.media.length;
+    for (var i = 0; i < len; i++){
+      if (this.state.media[i].mediaType === "Video"){
+        latestVideoID = i;
+      }
+    }
+    if (latestVideoID != null){
+      return (
+        <div className="md:w-11/12 w-full lg:rounded-full rounded-lg bg-red-900 bg-opacity-90 my-4 py-3 flex text-center justify-center">
+          <div className="items-center content-center justify-center text-center flex flex-row md:flex-nowrap flex-wrap px-3 lg:w-10/12 w-full">
+            <div className="w-auto text-left float-left pr-3">
+              <h2 className="font-semibold text-3xl py-3 text-white">Latest JCR Video</h2>
+              <h2 className="font-semibold text-xl pt-3 pb-1 text-white">{this.state.media[latestVideoID].mediaTitle}</h2>
+              <p className="w-auto font-light pb-3 pt-1 text-white flex-shrink">{this.state.media[latestVideoID].mediaDescription}</p>
+            </div>
+            <div className="w-auto">
+              {this.getVideoiFrameFromPartialUrl(this.state.media[latestVideoID].mediaLink)}
+            </div>
+          </div>
+        </div>
+      );
+    }
+    else { return <></> }
+  }
+
+  showSelectedItem(){
+    if (this.state.selectedMediaIndex === null){ return <></> }
+    const media = this.state.media[this.state.selectedMediaIndex]
+    if(media.mediaType === "Video"){
+      return (
+        <div className="md:w-11/12 w-full lg:rounded-full rounded-lg bg-red-900 bg-opacity-90 my-4 py-3 flex text-center justify-center">
+          <div className="items-center content-center justify-center text-center flex flex-row md:flex-nowrap flex-wrap px-3 lg:w-10/12 w-full">
+            <div className="w-auto text-left float-left pr-3">
+              <h2 className="font-semibold text-3xl py-3 text-white">Selected Video</h2>
+              <h2 className="font-semibold text-xl pt-3 pb-1 text-white">{media.mediaTitle}</h2>
+              <p className="w-auto font-light pb-3 pt-1 text-white flex-shrink">{media.mediaDescription}</p>
+            </div>
+            <div className="w-auto">
+              {this.getVideoiFrameFromPartialUrl(media.mediaLink)}
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="md:w-11/12 w-full lg:rounded-full rounded-lg bg-red-900 bg-opacity-90 my-4 py-3 flex text-center justify-center">
+        <div className="text-center w-10/12">
+            <h2 className="font-semibold text-3xl py-3 text-white">Selected Podcast</h2>
+            <h2 className="font-semibold text-xl pt-3 pb-1 text-white">{media.mediaTitle}</h2>
+            {this.getiFrameFromPartialUrl(media.mediaLink)}
+            <p className="w-auto font-light pb-3 pt-1 text-white flex-shrink">{media.mediaDescription}</p>
+          </div>
+      </div>
+    );
+  }
+
+  selectItem(id) {
+    if (this.state.selectedMediaIndex === id){
+      this.setState({ selectedMediaIndex: null });
+    }
+    else{
+      this.setState({ selectedMediaIndex: id });
+    }
+  }
+
+  getMediaList(){
+    return(
+      <div>
+        <div className="flex flex-col items-center w-full">
+          <h2 className="font-semibold text-4xl pt-3">All Media</h2>
+          <p className="pb-4 font-light text-l">"More media", you say? Click an item from the list below:</p>
+          {this.state.media.map((media, i) => (
+            <button
+              onClick={() => this.selectItem(i)}
+              className="md:w-8/12 w-11/12 mb-3 px-1 py-1 rounded-lg hover:bg-red-900 hover:text-white"
+            >
+              <div className="p-2 border-b border-red-900 flex flex-row justify-evenly items-center">
+                <h2 className="w-80 font-semibold text-3xl">{media.mediaTitle}</h2>
+                <p className="w-48 font-light text-xl">{media.mediaCategory}</p>
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    )
   }
 
   render () {
@@ -115,10 +232,18 @@ class MediaPage extends React.Component {
     }
 
     return (
-      <div className="flex flex-col justify-start">
-        <div className="container mx-auto text-center p-4">
+      <div className="flex flex-col justify-center w-full">
+        <div className="mx-0 text-center pt-4">
           <h1 className="font-semibold text-5xl pb-4">Media</h1>
           {this.state.media.length > 0 ? this.getLatestJCRPodcast() : <></>}
+          {this.state.media.length > 0 ? this.getLatestGMPodcast() : <></>}          
+        </div>
+        <div className="text-center flex justify-center w-full px-4">
+          {this.state.media.length > 0 ? this.getLatestVideo() : <></>}
+        </div>
+        <div className="text-center flex flex-col justify-center items-center w-full px-4">
+          {this.state.media.length > 0 ? this.getMediaList() : <p>No Media</p>}
+          {this.showSelectedItem()}
         </div>
       </div>
     );
