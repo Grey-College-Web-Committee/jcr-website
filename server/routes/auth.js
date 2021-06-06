92a97ffbebcd57028f59c414a2aea4973c5392d6
fboyle2001 <finlayboyle2001@gmail.com>
2021-06-06 17:06:38 +0100

    Removed test username

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -27,7 +27,7 @@ router.post("/login", async (req, res) => {
   // https://www.dur.ac.uk/its/password/validator
   // Providing headers 'Authorization' = 'Basic {{base64 encoded string 'username:password'}}'
 
-  if(username !== "nonmem" && username !== "test11") {
+  if(username !== "nonmem") {
     const details = Buffer.from(`${username}:${password}`);
     const b64data = details.toString("base64");
     const authHeader = `Basic ${b64data}`;
