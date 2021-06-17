bcf896142ea5946dd0fcef017e51b0158ed01c66
fboyle2001 <finlayboyle2001@gmail.com>
2021-06-17 17:40:28 +0100

    Started homepage

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -2,32 +2,54 @@ import React from 'react';
 import authContext from '../../utils/authContext.js';
 
 class HomePage extends React.Component {
+  getSlideshowPictures = () => {
+    return [
+      {
+        "desktop_title": "Grey College Charity Fashion Show",
+        "mobile_title": "GCCFS",
+        "description_text": "Find out more about the annual charity fashion show",
+        "button_text": "Find out more →",
+        "button_link": "https://www.gccfs.co.uk/",
+        "image": "/images/home/gccfs_1900_600.png"
+      }
+    ];
+  }
+
   render () {
     const loggedIn = this.context !== undefined && this.context !== null;
 
-    return (
-      <div className="flex justify-center items-center">
-        <div className="container mx-auto text-center p-4">
-          <div className="flex flex-row justify-center">
-            <div className="flex flex-col align-middle">
-              <div>
-                <h1 className="font-semibold text-5xl pb-4 text-center">Grey College JCR</h1>
-              </div>
-              <div className="text-2xl">
-                <p>Welcome to the new Grey College JCR website!</p>
-              </div>
-              {loggedIn ? null : (
-                <div className="text-xl">
-                  <p>To get started, login using your Durham CIS account.</p>
-                </div>
-              )}
-              <img
-                src="/images/grey_crest.svg"
-                alt="Grey College Crest"
-                className="pt-4 w-4/5 mx-auto"
-              />
-            </div>
+    const temp = this.getSlideshowPictures()[0];
+
+    /**
+    <div className="flex justify-center items-center">
+      <div className="container mx-auto text-center p-4">
+        <div className="flex flex-row justify-center">
+          <div>
           </div>
+        </div>
+      </div>
+    </div>
+    */
+
+    return (
+      <div className="relative">
+        <img
+          src={temp.image}
+          className="w-full h-auto relative z-0"
+        ></img>
+      <div className="absolute p-4 z-10 top-0 left-0 w-full h-full flex flex-row justify-between text-white items-center text-2xl">
+        <div>
+          ←
+        </div>
+        <div>
+          →
+        </div>
+      </div>
+        <div className="absolute p-4 z-10 bottom-0 left-0 w-full h-auto flex flex-col items-end text-white bg-grey-500 bg-opacity-75 border-t-4 border-b-4 border-red-900 justify-between">
+          <h2 className="font-bold text-4xl">{temp.desktop_title}</h2>
+          <button
+            className="font-semibold p-2 text-2xl mt-4 bg-red-900 bg-opacity-50 hover:bg-opacity-75 transition-all border-grey-500 border-4 border-opacity-50 hover:border-opacity-25 hover:border-red-800 focus:outline-none rounded-lg"
+          >{temp.button_text}</button>
         </div>
       </div>
     );
