fcc9f32430a869df0207752f222dc81e82257f30
fboyle2001 <finlayboyle2001@gmail.com>
2021-06-18 12:32:06 +0100

    More homepage changes but need to work on PF

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -37,19 +37,22 @@ class HomePage extends React.Component {
           src={temp.image}
           className="w-full h-auto relative z-0"
         ></img>
-      <div className="absolute p-4 z-10 top-0 left-0 w-full h-full flex flex-row justify-between text-white items-center text-2xl">
-        <div>
-          ←
-        </div>
-        <div>
-          →
-        </div>
-      </div>
-        <div className="absolute p-4 z-10 bottom-0 left-0 w-full h-auto flex flex-col items-end text-white bg-grey-500 bg-opacity-75 border-t-4 border-b-4 border-red-900 justify-between">
+      <div className="absolute p-4 z-20 bottom-0 left-0 w-full h-auto flex flex-col items-end text-white bg-grey-500 bg-opacity-75 border-t-4 border-b-4 border-red-900 justify-between">
           <h2 className="font-bold text-4xl">{temp.desktop_title}</h2>
           <button
-            className="font-semibold p-2 text-2xl mt-4 bg-red-900 bg-opacity-50 hover:bg-opacity-75 transition-all border-grey-500 border-4 border-opacity-50 hover:border-opacity-25 hover:border-red-800 focus:outline-none rounded-lg"
+            className="font-semibold p-2 text-2xl mt-4 bg-red-900 bg-opacity-50 hover:bg-opacity-75 transition-all focus:outline-none rounded-lg hover:shadow-inner"
           >{temp.button_text}</button>
+        </div>
+        <div className="absolute z-10 top-0 left-0 w-full h-full flex flex-row justify-between text-white items-center text-2xl hover:z-30">
+          <div className="h-full hover:bg-grey-500 hover:bg-opacity-25 cursor-pointer p-4 flex flex-col justify-center">
+            ←
+          </div>
+          <div className="h-full hover:bg-grey-500 hover:bg-opacity-25 cursor-pointer p-4 flex flex-col justify-center">
+            →
+          </div>
+        </div>
+        <div className="absolute z-10 top-0 left-0 w-full h-auto flex flex-row justify-center text-white items-center">
+          A
         </div>
       </div>
     );
