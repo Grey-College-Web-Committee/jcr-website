f8f1f37750a4e4b221466824bd9cc0a2ce90fd7f
fboyle2001 <finlayboyle2001@gmail.com>
2021-10-29 23:33:24 +0100

    Send local emails on production

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,44 +1,59 @@
 const nodemailer = require("nodemailer");
 
-// Creates the transporter to send the mail
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secureConnection: process.env.EMAIL_SECURE,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD
-  },
-  tls: {
-    ciphers: "SSLv3"
-  },
-  pool: true,
-  maxConnections: 3,
-  maxMessages: 100
-});
+const footer = [
+  '<div style="text-align: left; font-size: small; font-style: italic;">',
+  "<p>This is an automatically generated email. Replies to this email address are not monitored.</p>",
+  "<p>If you have any queries please contact the JCR Website Editor at grey.website@durham.ac.uk</p>",
+  "</div>"
+].join("");
 
-const sendEmail = async (to, subject, html) => {
-  return true;
+// Creates the transporter to send the mail
+
+let transporter;
+
+// Due to the uni forcing MFA we moved to postfix on the server in production
+if(process.env.LOCAL_EMAIL) {
+  transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secureConnection: process.env.EMAIL_SECURE,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    },
+    pool: true,
+    maxConnections: 3,
+    maxMessages: 100
+  });
 }
 
-// Changes to email means we can no longer connect so attempting to
-// send emails is pointless
 // Send an email using the transporter we have constructed
-// const sendEmail = async (to, subject, html) => {
-//   let info;
+const sendEmail = async (to, subject, html) => {
+  let info;
 
-//   try {
-//     info = await transporter.sendMail({
-//       from: process.env.EMAIL_SENDER,
-//       to,
-//       subject,
-//       html
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
+  const sendingHtml = html + footer;
 
-//   return info;
-// }
+  try {
+    info = await transporter.sendMail({
+      from: process.env.EMAIL_SENDER,
+      to,
+      subject,
+      html: sendingHtml
+    });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+
+  return info;
+}
 
 module.exports.sendEmail = sendEmail;
