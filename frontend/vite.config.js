9dec4008e9d8208a3aa821364db641811a6e3828
Jacob Wrenn <jacob@wrenn.me>
2023-06-13 10:27:27 +0100

    make sure dev server proxies correctly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:9000'
+      '/api': 'http://localhost:9000',
+      '^/uploads/.*': 'http://localhost:9000',
+      '^/elections/manifesto/.*': 'http://localhost:9000'
     },
     open: true
   }
